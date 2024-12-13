8a53f7539fbfdd921a50e7c9805af3a5298fe6b9
Svetlana Malashenko <svetlana.malashenko@logx.ru>
2024-12-13 15:31:35 +0300

    implemented themes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,20 @@
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+
+import { useMemo } from 'react'
+import { useSelector } from 'react-redux'
+import { themeSettings } from './theme'
+
 function App() {
-	return <>scscs</>
+	const mode = useSelector(state => state.global.mode)
+
+	const theme = useMemo(() => {
+		return createTheme(themeSettings(mode))
+	}, [mode])
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+		</ThemeProvider>
+	)
 }
 
 export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
+import { configureStore } from '@reduxjs/toolkit'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import globalReducer from '../src/state'
 import App from './App.jsx'
 import './index.css'
 
+const store = configureStore({
+	reducer: {
+		global: globalReducer,
+	},
+})
+
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</StrictMode>
 )
