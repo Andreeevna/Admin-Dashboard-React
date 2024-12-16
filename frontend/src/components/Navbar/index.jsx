8599dce1d4311eda4264e57c2032998a8931318f
Svetlana Malashenko <ms.andreeevna@yandex.ru>
2024-12-16 16:44:40 +0300

    added in Navbar profile

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,18 +1,40 @@
 import {
+	ArrowDropDownOutlined,
 	DarkModeOutlined,
 	LightModeOutlined,
 	Search,
 	SettingsOutlined,
 } from '@mui/icons-material'
 import MenuIcon from '@mui/icons-material/Menu'
-import { AppBar, IconButton, InputBase, Toolbar, useTheme } from '@mui/material'
+import {
+	AppBar,
+	Box,
+	Button,
+	IconButton,
+	InputBase,
+	Toolbar,
+	Typography,
+	useTheme,
+} from '@mui/material'
+import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { setMode } from '../../state/index.js'
 import FlexBetween from '../FlexBetween.jsx'
 
-const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
+import profileImage from './../../assets/profile.png'
+
+const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
 	const dispatch = useDispatch()
 	const theme = useTheme()
+
+	const [anchorEl, setAnchorEl] = useState(null)
+	const isOpen = Boolean(anchorEl)
+
+	const handleClick = event => {
+		setAnchorEl(event.currentTarget)
+	}
+
+	const handleClose = () => setAnchorEl(null)
 	return (
 		<AppBar
 			sx={{
@@ -67,6 +89,46 @@ const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
 							}}
 						/>
 					</IconButton>
+					<FlexBetween>
+						<Button
+							onClick={handleClick}
+							sx={{
+								display: 'flex',
+								justifyContent: 'space-between',
+								alignItems: 'center',
+								textTransform: 'none',
+								gap: '1rem',
+							}}
+						>
+							<Box
+								component='img'
+								alt='profile'
+								src={profileImage}
+								height='32px'
+								width='32px'
+								borderRadius='50%'
+								sx={{ objectFit: 'cover' }}
+							/>
+							<Box textAlign='left'>
+								<Typography
+									fontWeight='bold'
+									fontSize='0.85rem'
+									sx={{ color: theme.palette.secondary[100] }}
+								>
+									{user.name}
+								</Typography>
+								<Typography
+									fontSize='0.75rem'
+									sx={{ color: theme.palette.secondary[200] }}
+								>
+									{user.occupation}
+								</Typography>
+							</Box>
+							<ArrowDropDownOutlined
+								sx={{ color: theme.palette.secondary[300], fontSize: '25px' }}
+							/>
+						</Button>
+					</FlexBetween>
 				</FlexBetween>
 			</Toolbar>
 		</AppBar>
