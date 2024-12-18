a891d76e0ff61c7591d08177cdc8a1bf855e66dc
Svetlana Malashenko <ms.andreeevna@yandex.ru>
2024-12-18 11:13:35 +0300

    create header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Dashboard from './scenes/dashboard'
 import Layout from './scenes/layout'
+import Products from './scenes/products'
 import { themeSettings } from './theme'
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
 					<Route element={<Layout />}>
 						<Route path='/' element={<Navigate to='/dashboard' replace />} />
 						<Route path='/dashboard' element={<Dashboard />}></Route>
+						<Route path='/products' element={<Products />}></Route>
 					</Route>
 				</Routes>
 			</ThemeProvider>
