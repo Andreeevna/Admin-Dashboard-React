2d65edce910f99358af76fc078967d5eaf381e05
Svetlana Malashenko <ms.andreeevna@yandex.ru>
2024-12-18 10:06:06 +0300

    created ProductSchema and ProductStateSchema

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -12,6 +12,8 @@ import {
 	Button,
 	IconButton,
 	InputBase,
+	Menu,
+	MenuItem,
 	Toolbar,
 	Typography,
 	useTheme,
@@ -128,6 +130,14 @@ const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
 								sx={{ color: theme.palette.secondary[300], fontSize: '25px' }}
 							/>
 						</Button>
+						<Menu
+							anchorEl={anchorEl}
+							open={isOpen}
+							onClose={handleClose}
+							anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+						>
+							<MenuItem onClick={handleClose}>Log Out</MenuItem>
+						</Menu>
 					</FlexBetween>
 				</FlexBetween>
 			</Toolbar>
